9ecdc953f4d6771b8798da6e6036d79256c53c75
Hasanuddin <118762684+Virtamos@users.noreply.github.com>
2024-09-25 10:32:01 +0530

    Add files via upload

---
+++ queen.py
@@ -0,0 +1,37 @@
+class solution:
+    def __init__(self):
+        self.MAX = 20
+        self.A = [0]*self.MAX
+
+    def placement(self,i,j):
+        for k in range(1,i):
+            if(self.A[k] ==j) or abs(self.A[k] -j) ==abs(k-i):
+                return False
+        print(self.A)
+        return True
+
+    def printplacedqueen(self,N):
+        print('Arrangement--->')
+        print()
+
+        for i in range(1,N+1):
+            for j in range(1,N+1):
+                if self.A[i]!=j:
+                    print('\t_', end=' ')
+                else:
+                    print('\tQ', end=' ')
+            print()
+            print()
+
+    def N_Queens(self,i,j):
+        for k in range(1,N+1):
+            if self.placement(i,k):
+                self.A[i]=k
+                if i==N:
+                    self.printplacedqueen(N)
+                else:
+                    self.N_Queens(i+1,N)
+
+N= int(input("Enter queen's value"))
+obj = solution()
+obj.N_Queens(1,N)
